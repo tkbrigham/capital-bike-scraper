cf502a3fd71ea24d2676c1d9776c441b4c5f8ff3
Thomas Brigham <Thomas@Thomass-MacBook-Pro.local>
2015-03-22 17:01:16 -0400

    #load_new_stations seems to be working.

--- app/models/capital_scraper.rb
+++ app/models/capital_scraper.rb
@@ -30,31 +30,41 @@ class CapitalScraper
 
   def self.format_field(element)
     formatted_field = element.text
-
     formatted_field = element.text.to_f if float_fields.include?(element.name)
     formatted_field = element.text.to_i if integer_fields.include?(element.name)
     formatted_field = Time.at(element.text[0..9].to_i) if datetime_fields.include?(element.name)
     formatted_field
   end
 
-  def self.load_station(station)
-    s = Station.new()
-    station.children.each do |element|
-      print element.name, ', '
-      print "Class = #{format_field(element).class}", ', '
-      puts format_field(element)
-      s[cb(element.name)] = format_field(element) unless ['nbBikes','nbEmptyDocks'].include?(element.name)
+  def self.existing_station_ids
+    station_ids = []
+    Station.find_each do |s|
+      station_ids << s.id
     end
-    puts s.attributes
-    #s.save
-  end
-  
-  def self.first_station
-    all_stations.first
+    station_ids
   end
 
-  def self.test
-    load_station_stats(first_station)
+  def self.cb_station_ids
+    station_ids = []
+    all_stations.each do |s|
+      station_ids << s.css('id').first.text.to_i
+    end
+    station_ids
+  end
+  
+  def self.new_stations
+    new_stations = []
+    new_station_ids = []
+    for i in (existing_station_ids.max + 1)..cb_station_ids.max
+      new_station_ids << i
+    end
+    all_stations.each do |s|
+      if new_station_ids.include?(s.css('id').first.text.to_i)
+        puts s
+        new_stations << s
+      end
+    end
+    new_stations
   end
 
   def self.load_station_stats(station)
@@ -64,13 +74,30 @@ class CapitalScraper
     ss['cb_nbBikes'] = station.css('nbBikes').first.text
     ss['cb_nbEmptyDocks'] = station.css('nbEmptyDocks').first.text
     puts ss.attributes
+    ss
   end
 
-  def self.load_all_stations
-    all_stations.each do |station|
-      load_station(station)
+  def self.load_new_stations
+    puts "New stations:"
+    new_stations.each do |s|
+      puts s.css('id').first.text
+    end
+    puts "Want to add?"
+    add = gets.chomp
+    if add == 'y'
+      new_stations.each do |station|
+        load_station(station)
+      end
     end
     return "Finished!"
+  end
+
+  def self.load_station(station)
+    s = Station.new()
+    station.children.each do |element|
+      s[cb(element.name)] = format_field(element) unless ['nbBikes','nbEmptyDocks'].include?(element.name)
+    end
+    s.save
   end
 
 end
